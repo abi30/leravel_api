45459139dcc014ed56db0369bdbde4a28109975c
abi30 <rakib0751@gmail.com>
2022-02-07 22:59:56 +0100

    work on tinker

--- app/Models/model/Product.php
+++ app/Models/model/Product.php
@@ -4,8 +4,15 @@ namespace App\Models\model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\model\Review;
 
 class Product extends Model
 {
+
+     public function reviews(){
+         return $this->hasMany(Review::class);
+     }
+
+
     use HasFactory;
 }
--- app/Models/model/Review.php
+++ app/Models/model/Review.php
@@ -4,8 +4,14 @@ namespace App\Models\model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\model\Product;
 
 class Review extends Model
 {
-    use HasFactory;
+    public function product()
+    {
+
+        return $this->belongsTo(Product::class);
+
+    }
 }
