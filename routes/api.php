e7a187f832a5fc325ffe9de775fe027d9b1f7e6c
abi30 <rakib0751@gmail.com>
2022-02-07 20:51:53 +0100

    start api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/products',ProductController::class);
+// Route::apiResource('/reviews',ReviewController::class);
+
+// /products/id:43/reviews
+
+Route::group(['prefix'=>'products'],function(){
+
+    Route::apiResource('/{product}/reviews',ReviewController::class);
+
+ });
